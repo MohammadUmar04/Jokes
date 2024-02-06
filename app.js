1ba21ca116d32a4272148ef33a125b2f921842c5
MohammadUmar04 <muhammadomar009290@gmail.com>
2024-02-06 12:39:15 +0500

    i made a joke page

---
+++ app.js
@@ -0,0 +1,342 @@
+var jokes = [
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "A man is smoking a cigarette and blowing smoke rings into the air. His girlfriend becomes irritated with the smoke and says \"Can't you see the warning on the cigarette pack? Smoking is hazardous to your health!\" to which the man replies, \"I am a programmer.  We don't worry about warnings; we only worry about errors.\"",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 38,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "// This line doesn't actually do anything, but the code stops working when I delete it.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 12,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "Knock knock.\nWho's there?\nRecursion.\nRecursion who?\nKnock knock.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 180,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "Java is like Alzheimer's, it starts off slow, but eventually, your memory is gone.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 27,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Misc",
+            "type": "single",
+            "joke": "Relationship Status: just tried to reach for my dog's paw and he pulled it away so I pretended I was reaching for the remote.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "racist": false,
+                "sexist": false,
+                "political": false,
+                "explicit": false
+            },
+            "id": 195,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "\"Honey, go to the store and buy some eggs.\"\n\"OK.\"\n\"Oh and while you're there, get some milk.\"\nHe never returned.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 18,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "Hey Girl,\nRoses are #ff0000,\nViolets are #0000ff,\nI use hex codes,\nBut I'd use RGB for you.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 41,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "Algorithm: A word used by programmers when they don't want to explain how their code works.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 51,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "I'd tell you a joke about NAT but I would have to translate.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "safe": true,
+            "id": 319,
+            "lang": "en"
+        },
+        {
+            "category": "Pun",
+            "type": "single",
+            "joke": "A horse walks into a bar.\n\"Hey\", the Bartender says.\n\"Sure\", the horse replies.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 74,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "error": false,
+            "amount": 10,
+            "jokes": [
+                {
+                    "category": "Programming",
+                    "type": "single",
+                    "joke": "// This line doesn't actually do anything, but the code stops working when I delete it.",
+                    "flags": {
+                        "nsfw": false,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": false,
+                        "explicit": false
+                    },
+                    "id": 12,
+                    "safe": true,
+                    "lang": "en"
+                },
+                {
+                    "category": "Programming",
+                    "type": "twopart",
+                    "setup": "Why was the JavaScript developer sad?",
+                    "delivery": "Because they didn't Node how to Express themself!",
+                    "flags": {
+                        "nsfw": false,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": false,
+                        "explicit": false
+                    },
+                    "safe": true,
+                    "id": 292,
+                    "lang": "en"
+                },
+                {
+                    "category": "Programming",
+                    "type": "single",
+                    "joke": "Documentation is like sex:\nWhen it's good, it's very good.\nWhen it's bad, it's better than nothing...",
+                    "flags": {
+                        "nsfw": true,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": false,
+                        "explicit": false
+                    },
+                    "safe": false,
+                    "id": 305,
+                    "lang": "en"
+                },
+                {
+                    "category": "Programming",
+                    "type": "twopart",
+                    "setup": "What is the most used language in programming?",
+                    "delivery": "Profanity.",
+                    "flags": {
+                        "nsfw": false,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": false,
+                        "explicit": false
+                    },
+                    "id": 193,
+                    "safe": true,
+                    "lang": "en"
+                },
+                {
+                    "category": "Programming",
+                    "type": "single",
+                    "joke": "Your mama's so FAT she can't save files bigger than 4GB.",
+                    "flags": {
+                        "nsfw": false,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": false,
+                        "explicit": true
+                    },
+                    "id": 9,
+                    "safe": false,
+                    "lang": "en"
+                },
+                {
+                    "category": "Programming",
+                    "type": "twopart",
+                    "setup": "Why did the Python data scientist get arrested at customs?",
+                    "delivery": "She was caught trying to import pandas!",
+                    "flags": {
+                        "nsfw": false,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": false,
+                        "explicit": false
+                    },
+                    "id": 234,
+                    "safe": true,
+                    "lang": "en"
+                },
+                {
+                    "category": "Programming",
+                    "type": "twopart",
+                    "setup": "Why did the functional programmer get thrown out of school?",
+                    "delivery": "Because he refused to take classes.",
+                    "flags": {
+                        "nsfw": false,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": false,
+                        "explicit": false
+                    },
+                    "id": 48,
+                    "safe": true,
+                    "lang": "en"
+                },
+                {
+                    "category": "Programming",
+                    "type": "twopart",
+                    "setup": "How did the programmer die in the shower?",
+                    "delivery": "He read the shampoo bottle instructions: Lather. Rinse. Repeat.",
+                    "flags": {
+                        "nsfw": false,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": false,
+                        "explicit": false
+                    },
+                    "safe": false,
+                    "id": 266,
+                    "lang": "en"
+                },
+                {
+                    "category": "Programming",
+                    "type": "single",
+                    "joke": "Four engineers get into a car. The car won't start.\nThe Mechanical engineer says \"It's a broken starter\".\nThe Electrical engineer says \"Dead battery\".\nThe Chemical engineer says \"Impurities in the gasoline\".\nThe IT engineer says \"Hey guys, I have an idea: How about we all get out of the car and get back in\".",
+                    "flags": {
+                        "nsfw": false,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": false,
+                        "explicit": false
+                    },
+                    "id": 132,
+                    "safe": true,
+                    "lang": "en"
+                },
+                {
+                    "category": "Programming",
+                    "type": "twopart",
+                    "setup": "Genders are a lot like booleans.",
+                    "delivery": "There's only two of them.",
+                    "flags": {
+                        "nsfw": false,
+                        "religious": false,
+                        "political": false,
+                        "racist": false,
+                        "sexist": true,
+                        "explicit": false
+                    },
+                    "id": 47,
+                    "safe": false,
+                    "lang": "en"
+                }
+            ]
+        }
+    ]
+
+    let index = Math.floor(Math.random() * jokes.length);
+    document.getElementById("manyjokes").innerHTML = jokes[index].joke;
+        
+
+
+
+
